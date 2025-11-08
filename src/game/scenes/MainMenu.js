b96ed9ef05d365b340d13e416a542c8ebf67f971
KarlPham <imouli819@gmail.com>
2025-11-08 20:21:58 +1100

    fix(Menu): update the start button in the center of the scene

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -9,6 +9,7 @@ import { Scene } from 'phaser';
     // Load background and button assets
     this.load.image('menu_bg', 'assets/images/menu/menu_bg.jpg');
     this.load.image('start_btn', 'assets/images/menu/start_btn_1.png');
+    this.load.audio('theme_song', 'assets/audio/theme_song.mp3');
     }
 
     create() {
@@ -17,9 +18,11 @@ import { Scene } from 'phaser';
     bg.displayWidth = this.sys.game.config.width;
     bg.displayHeight = this.sys.game.config.height;
 
+    this.sound.play('theme_song');
+
     // Add and center Start button
     const { width, height } = this.sys.game.config;
-    const startBtn = this.add.image(width/2, height/2.5, 'start_btn').setInteractive();
+    const startBtn = this.add.image(475, height/2.5, 'start_btn').setInteractive();
     startBtn.setScale(0.2); // Scale as needed for appearance
 
     // Clicking the button starts the Game scene
