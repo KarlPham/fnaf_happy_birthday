9ce6941ad49b83daf3b1148be764e4091bea8936
KarlPham <imouli819@gmail.com>
2025-11-08 23:30:43 +1100

    feat(docs): build for deployment

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
+    base: '/fnaf_happy_birthday/',
     build: {
         rollupOptions: {
             output: {
